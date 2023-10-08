4b35e46dc29eb4dd82b0c0f61b75e768639b19e1
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-08 04:06:41 +0100

    e

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """adv task"""
-from typing import Tuple, Iterable, Union
+from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """zoom array"""
-    zoomed_in: Tuple = tuple([
+    zoomed_in: List = [
         item for item in lst
-        for i in range(int(factor))
-    ])
+        for i in range(factor)
+    ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
-zoom_2x = zoom_array(tuple(array))
+zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(tuple(array), 3)
+zoom_3x = zoom_array(array, 3)
