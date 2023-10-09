e83a48e347523b755ac9c1760cc12e10036c9b78
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-09 20:07:49 +0100

    measure

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""asyncio example"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+
+def measure_time(n, max_delay):
+    """measure time of wait_n async function"""
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    stop = time.time()
+    return (stop - start) / n
