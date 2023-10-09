4f77d97c8b2df608af19125883210221b30568a1
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-09 13:02:46 -0700

    make truely concurrently

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List:
     """concurrent coroutines"""
     delays: List = []
-    for i in range(n):
-        delays.append(await asyncio.create_task(wait_random(max_delay)))
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    for task in tasks:
+        delays.append(await task)
     delays.sort()
     return delays
