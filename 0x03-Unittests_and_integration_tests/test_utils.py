46dcf2e1569ae3b0bf557132f3afd1d5f1191568
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-30 21:14:58 +0100

    idk tf is happening

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,11 +3,15 @@
 Test the utils.py module
 """
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test access nested map function
+    """
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -29,3 +33,24 @@ class TestAccessNestedMap(unittest.TestCase):
         Test access_nested_map raises error with wrong parameters"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test get_json function
+    """
+    @patch('module_name.requests.get')
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload, mock_requests_get):
+        mock_requests = Mock()
+        mock_requests.json.return_value = test_payload
+        
+        mock_requests_get.return_value = mock_requests
+
+        result = get_json(test_url)
+
+        mock_requests_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
