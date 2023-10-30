31d3a767a3e39c2fa8db26c214a3ab4e6034c3b2
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-10-30 17:35:37 +0100

    test access_nested_map exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -18,3 +19,13 @@ class TestAccessNestedMap(unittest.TestCase):
         Test access_nested_map function
         """
         self.assertEqual(access_nested_map(nested_map, path), result)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Test access_nested_map raises error with wrong parameters"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
